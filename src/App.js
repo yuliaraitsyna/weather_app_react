9cb8a4d388aaba999496ba9eac5863fedfda3a84
yuliaraitsyna <yuliaraitsyna@gmail.com>
2024-04-04 22:20:45 +0300

    first api commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import CurrentForcast from "./components/current_forcast/CurrentForcast.jsx";
 import DayForcast from "./components/week_forcast/DayForcast.jsx";
+import { useEffect, useState } from "react";
 import "./styles/App.css"
 
 const DAYS = {
@@ -13,6 +14,14 @@ const DAYS = {
     Sun: 6
 }
 const App = () => {
+    const [data, setData] = useState(null);
+        useEffect(() => {
+            fetch('http://api.weatherapi.com/v1/current.json?key=fd14bf79a3bd4f40981191408240404&q=Minsk&aqi=no')
+            .then(response => response.json())
+            .then(json => setData(json))
+            .catch(error => console.error(error));
+        }, []);
+    console.log(data);
     return (
         <div id="App">
             <CurrentForcast></CurrentForcast>
--- src/components/current_forcast/CurrentForcast.jsx
+++ src/components/current_forcast/CurrentForcast.jsx
@@ -1,7 +1,12 @@
+import { useEffect, useState } from "react"
 import "../../styles/current_forcast.css"
 export default function CurrentForcast () {
+    function getRequestData() {
+        
+    }
     return (
         <div id="current-forcast-container">
+            
             <form id="search-bar">
                 <input type="search" placeholder="Find location"></input>
                 <button type="submit"></button>
