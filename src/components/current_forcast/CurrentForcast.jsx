87c83a956aa7bf69c8ad922ccbb890dc36068c45
yuliaraitsyna <yuliaraitsyna@gmail.com>
2024-04-17 10:45:53 +0300

    implement display of current forcast

--- src/components/current_forcast/CurrentForcast.jsx
+++ src/components/current_forcast/CurrentForcast.jsx
@@ -1,24 +1,21 @@
 import { useEffect, useState } from "react"
 import "../../styles/current_forcast.css"
-export default function CurrentForcast () {
-    function getRequestData() {
-        
-    }
+export default function CurrentForcast (props) {
+    console.log(props);
     return (
         <div id="current-forcast-container">
-            
             <form id="search-bar">
                 <input type="search" placeholder="Find location"></input>
                 <button type="submit"></button>
             </form>
-            <img src=""></img>
-            <span>Temperatue</span>
+            <img id="current-forcast-icon" name="current-forcast"></img>
+            <span id="temperature-display" name="temperature">{props.data.temp_c}</span>
             <div id="info">
-                <span>Wind</span>
-                <span>Humidity</span>
-                <span>UV</span>
+                <span id="wind-speeed-display" name="wind-speed">{props.data.wind_speed}</span>
+                <span id="humidity-display" name="himidity"><img></img>{props.data.humidity}</span>
+                <span id="uv-display" name="uv"><img></img>{props.data.uv}</span>
             </div>
-            <p>Location</p>
+            <p id="location-display" name="location"><img></img>{props.data.location}</p>
         </div>
     )
 }
