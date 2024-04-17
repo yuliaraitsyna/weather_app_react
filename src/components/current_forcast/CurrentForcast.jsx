aa9b9e651f4ef68ea6f3a977b61183690f595f53
yuliaraitsyna <yuliaraitsyna@gmail.com>
2024-04-17 12:02:16 +0300

    add ids and icons

--- src/components/current_forcast/CurrentForcast.jsx
+++ src/components/current_forcast/CurrentForcast.jsx
@@ -8,12 +8,12 @@ export default function CurrentForcast (props) {
                 <input type="search" placeholder="Find location"></input>
                 <button type="submit"></button>
             </form>
-            <img id="current-forcast-icon" name="current-forcast"></img>
-            <span id="temperature-display" name="temperature">{props.data.temp_c}</span>
+            <img id="current-forcast-icon" name="current-forcast" src={props.data.icon}></img>
+            <span id="temperature-display" name="temperature"><img id="temp_c-img"></img>{props.data.temp_c}</span>
             <div id="info">
-                <span id="wind-speeed-display" name="wind-speed">{props.data.wind_speed}</span>
-                <span id="humidity-display" name="himidity"><img></img>{props.data.humidity}</span>
-                <span id="uv-display" name="uv"><img></img>{props.data.uv}</span>
+                <span id="wind-speeed-display" name="wind-speed"><img id="wind_speed-img"></img>{props.data.wind_speed}</span>
+                <span id="humidity-display" name="himidity"><img id="humidity-img"></img>{props.data.humidity}</span>
+                <span id="uv-display" name="uv"><img id="uv-img"></img>{props.data.uv}</span>
             </div>
             <p id="location-display" name="location"><img></img>{props.data.location}</p>
         </div>
