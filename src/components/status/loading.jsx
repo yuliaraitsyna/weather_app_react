7631e35e26156cac097ebb4c947256dd59900ef1
yuliaraitsyna <yuliaraitsyna@gmail.com>
2024-04-21 22:17:11 +0300

    loading and error  handling

---
+++ src/components/status/loading.jsx
@@ -0,0 +1,9 @@
+import "../../styles/loading.css"
+export default function Loading() {
+    return (
+        <div id="loading">
+            <div className="loader"></div>
+            <p>Loading...</p>
+        </div>
+    )
+}
